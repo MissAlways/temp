73339d8e6ea8a1b4a2ee4baa585fb3f9669553fe
MissAlways <ainakiurus@gmail.com>
2016-10-18 21:39:26 +0300

    Add consoleTesting file that prints all players, player by id and print all items

---
+++ src/console/consoleTesting.java
@@ -0,0 +1,95 @@
+package console;
+
+import java.util.List;
+import java.util.Scanner;
+
+import bean.Item;
+import bean.Player;
+import db.ItemDao;
+import db.PlayerDao;
+
+/**
+ * 
+ * @author MissAlways
+ *
+ */
+public class consoleTesting {
+	private static String decoLine = "--------------------------------------";
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		try {
+			handleArguments(scan);
+		} catch (Exception exc) {
+			System.out.println(exc.getMessage());
+		} finally {
+			scan.close();
+			System.out.println("Cya later");
+		}
+	}
+
+	private static void handleArguments(Scanner scan) {
+		System.out.print("Select option " + "\n0. quit."
+				+ "\n1. print all players" + "\n2. print player by id"
+				+ "\n3. print all items" + "\nYour choice: ");
+		int choose = scan.nextInt();
+		scan.nextLine();
+
+		switch (choose) {
+		case 0: 
+			System.out.println("Bye!");
+			return;
+		case 1:
+			printAllPlayers();
+			break;
+		case 2:
+			System.out.print("Id: ");
+			int id = scan.nextInt();
+			scan.nextLine();
+			printById(id);
+			break;
+		case 3:
+			printAllItems();
+			break;
+		default:
+			System.out.println("Wrong number");
+			break;
+		}
+		handleArguments(scan);
+	}
+
+	private static void printAllItems() {
+		ItemDao itemDao = new ItemDao();
+		List<Item> itemList = itemDao.getAllItems();
+
+		if (itemList != null) {
+			// for(int i = 0; i < itemList.size(); i++){
+			for (Item item : itemList) {
+				System.out.println(item.toString());
+				System.out.println(decoLine);
+			}
+		}
+
+	}
+
+	private static void printById(int playerId) {
+		PlayerDao playerDao = new PlayerDao();
+		Player player = playerDao.getPlayer(playerId);
+
+		System.out.println(player.toString());
+	}
+
+	public static void printAllPlayers() {
+		PlayerDao playerDao = new PlayerDao();
+		List<Player> playerList = playerDao.getAllPlayers();
+
+		if (playerList != null) {
+			for (Player player : playerList) {
+				System.out.println(player.toString());
+				System.out.println(decoLine);
+			}
+		} else {
+			System.out.println("Playerlist empty");
+		}
+	}
+}
