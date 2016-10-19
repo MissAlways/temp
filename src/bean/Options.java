53e23c2ecda252ffeb22517fd9555ad0eb70e600
Aina Kiuru <ainakiurus@gmail.com>
2016-10-19 17:59:02 +0300

    Add Options-bean, OptionsDao and settings.ini

---
+++ src/bean/Options.java
@@ -0,0 +1,153 @@
+package bean;
+
+/**
+ * @author MissAlways
+ *
+ */
+public class Options {
+	private int resolutionX;
+	private int resolutionY;
+	private boolean fullscreen;
+	private boolean sound;
+	private int soundVolume;
+	private boolean music;
+	private int musicVolume;
+
+	/**
+	 * @param resolutionX horizontal resolution
+	 * @param resolutionY vertical resolution
+	 * @param fullscreen fullscreen on(true) or off(false)
+	 * @param sound sound on(true) or off(false)
+	 * @param soundVolume sound volume
+	 * @param music music on(true) or off(false)
+	 * @param musicVolume music volume
+	 */
+	public Options() {
+		resolutionX = 600;
+		resolutionY = 400;
+		fullscreen = false;
+		sound = true;
+		soundVolume = 100;
+		music = true;
+		musicVolume = 100;
+	}
+
+	/**
+	 * @return the resolutionX
+	 */
+	public int getResolutionX() {
+		return resolutionX;
+	}
+
+	/**
+	 * @param resolutionX
+	 *            the resolutionX to set
+	 */
+	public void setResolutionX(int resolutionX) {
+		this.resolutionX = resolutionX;
+	}
+
+	/**
+	 * @return the resolutionY
+	 */
+	public int getResolutionY() {
+		return resolutionY;
+	}
+
+	/**
+	 * @param resolutionY
+	 *            the resolutionY to set
+	 */
+	public void setResolutionY(int resolutionY) {
+		this.resolutionY = resolutionY;
+	}
+
+	/**
+	 * @return the fullscreen
+	 */
+	public boolean isFullscreen() {
+		return fullscreen;
+	}
+
+	/**
+	 * @param fullscreen
+	 *            the fullscreen to set
+	 */
+	public void setFullscreen(boolean fullscreen) {
+		this.fullscreen = fullscreen;
+	}
+
+	/**
+	 * @return the sound
+	 */
+	public boolean isSound() {
+		return sound;
+	}
+
+	/**
+	 * @param sound
+	 *            the sound to set
+	 */
+	public void setSound(boolean sound) {
+		this.sound = sound;
+	}
+
+	/**
+	 * @return the soundVolume
+	 */
+	public int getSoundVolume() {
+		return soundVolume;
+	}
+
+	/**
+	 * @param soundVolume
+	 *            the soundVolume to set
+	 */
+	public void setSoundVolume(int soundVolume) {
+		this.soundVolume = soundVolume;
+	}
+
+	/**
+	 * @return the music
+	 */
+	public boolean isMusic() {
+		return music;
+	}
+
+	/**
+	 * @param music
+	 *            the music to set
+	 */
+	public void setMusic(boolean music) {
+		this.music = music;
+	}
+
+	/**
+	 * @return the musicVolume
+	 */
+	public int getMusicVolume() {
+		return musicVolume;
+	}
+
+	/**
+	 * @param musicVolume
+	 *            the musicVolume to set
+	 */
+	public void setMusicVolume(int musicVolume) {
+		this.musicVolume = musicVolume;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Options [resolutionX=" + resolutionX + ", resolutionY="
+				+ resolutionY + ", fullscreen=" + fullscreen + ", sound="
+				+ sound + ", soundVolume=" + soundVolume + ", music=" + music
+				+ ", musicVolume=" + musicVolume + "]";
+	}
+
+}
---
+++ src/db/OptionsDao.java
@@ -0,0 +1,122 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
+import bean.Options;
+
+/**
+ * 
+ * @author MissAlways
+ *
+ */
+public class OptionsDao {
+
+	public Options getOptions() {
+		InputStream inputStream = null;
+		Properties properties = new Properties();
+		Options options = new Options();
+		String temp;
+
+		try {
+			inputStream = new FileInputStream("settings.ini");
+			properties.load(inputStream);
+			try {
+				temp = properties.getProperty("resolutionX");
+				options.setResolutionX(Integer.parseInt(temp));
+
+			} catch (Exception e) {
+				System.out.println("Horizontal resolution incorrect");
+			}
+			try {
+				temp = properties.getProperty("resolutionY");
+				options.setResolutionY(Integer.parseInt(temp));
+			} catch (Exception e) {
+				System.out.println("Vertical resolution incorrect");
+			}
+			try {
+				temp = properties.getProperty("fullscreen");
+				options.setFullscreen(Boolean.parseBoolean(temp));
+			} catch (Exception e) {
+				System.out.println("Fullscreen setting incorrect");
+			}
+			try {
+				temp = properties.getProperty("sound");
+				options.setSound(Boolean.parseBoolean(temp));
+			} catch (Exception e) {
+				System.out.println("Sound setting incorrect");
+			}
+			try {
+				temp = properties.getProperty("soundVolume");
+				options.setSoundVolume(Integer.parseInt(temp));
+			} catch (Exception e) {
+				System.out.println("SoundVolume setting incorrect");
+			}
+
+			try {
+				temp = properties.getProperty("music");
+				options.setMusic(Boolean.parseBoolean(temp));
+			} catch (Exception e) {
+				System.out.println("music setting incorrect");
+			}
+			try {
+				temp = properties.getProperty("musicVolume");
+				options.setMusicVolume(Integer.parseInt(temp));
+			} catch (Exception e) {
+				System.out.println("MusicVolume setting incorrect");
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (inputStream != null) {
+				try {
+					inputStream.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return options;
+	}
+
+	public void setOptions(Options options){
+		Properties properties = new Properties();
+		OutputStream outputStream = null;
+		String temp;
+		
+		try {
+			outputStream = new FileOutputStream("settings.ini");
+			temp = Integer.toString(options.getResolutionX());
+			properties.setProperty("resolutionX", temp);
+			temp = Integer.toString(options.getResolutionY());
+			properties.setProperty("resolutionY", temp);
+			temp = Boolean.toString(options.isFullscreen());
+			properties.setProperty("fullscreen", temp);
+			temp = Boolean.toString(options.isSound());
+			properties.setProperty("sound", temp);
+			temp = Integer.toString(options.getSoundVolume());
+			properties.setProperty("soundVolume", temp);
+			temp = Boolean.toString(options.isMusic());
+			properties.setProperty("music", temp);
+			temp = Integer.toString(options.getMusicVolume());
+			properties.setProperty("musicVolume", temp);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally{
+			if (outputStream != null) {
+				try {
+					outputStream.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
